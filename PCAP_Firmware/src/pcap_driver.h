790b9a6d616c64cd04a90029a5442e8a27204833
JoelManYunLee <joelmyleee@gmail.com>
2026-03-24 20:12:21 -0400

    Reduced SPI clock freq from 1 Mhz to 1kHz

--- PCAP_Firmware/src/pcap_driver.h
+++ PCAP_Firmware/src/pcap_driver.h
@@ -26,7 +26,7 @@ extern "C" {
  * @brief SPI bus settings for PCAP04 communication
  * @{
  */
-#define PCAP_SPI_CLOCK_HZ   1000000  ///< SPI clock frequency: 1 MHz
+#define PCAP_SPI_CLOCK_HZ   1000  ///< SPI clock frequency: 1 KHz
 #define PCAP_SPI_MODE       1        ///< SPI mode 1 (CPOL=0, CPHA=1)
 
 // XIAO ESP32C3 SPI pins
